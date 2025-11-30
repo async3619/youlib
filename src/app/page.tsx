b854bc88faf0b7e3e3daaf3679bcf736c63fc560
Sophia (Turner) <me@sophia-dev.io>
2025-11-30 11:06:35 +0900

    feat(auth): refactor authentication setup and add session provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,21 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
 import { SiteHeader } from '@/components/site-header'
-import { SessionProvider } from 'next-auth/react'
 
 export default function Home() {
   return (
-    <SessionProvider>
-      <SidebarProvider
-        style={
-          {
-            '--sidebar-width': 'calc(var(--spacing) * 72)',
-            '--header-height': 'calc(var(--spacing) * 12)',
-          } as React.CSSProperties
-        }
-      >
-        <AppSidebar variant="inset" />
-        <SidebarInset>
-          <SiteHeader />
-        </SidebarInset>
-      </SidebarProvider>
-    </SessionProvider>
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
